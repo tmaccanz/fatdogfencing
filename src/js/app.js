0523e397435999a42e4285d0928f266ca03432f5
ToddMcAlinden <tdjmcalinden@gmail.com>
2018-09-11 17:00:57 +1200

    form ajax fix

--- src/js/app.js
+++ src/js/app.js
@@ -333,5 +333,12 @@ let mySwiper = new Swiper('.swiper-container', {
     }
   }   
   
-
+  $(".contact-form").submit(function(e) {
+    e.preventDefault();
+  
+    var $form = $(this);
+    $.post($form.attr("action"), $form.serialize()).then(function() {
+      alert("Thank you!");
+    });
+  });
 
