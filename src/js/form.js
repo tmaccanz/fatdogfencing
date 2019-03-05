db27fbfb5fbf458ab94c199f9ac7d194355ea0a3
ToddMcAlinden <tdjmcalinden@gmail.com>
2019-03-06 01:25:11 +1300

    form update

--- src/js/form.js
+++ src/js/form.js
@@ -2,15 +2,27 @@ import $ from "jquery";
 
 export default function formModule (){
 
-	function submitForm(e) {
-		e.preventDefault();
-		const $form = $(this);
 
-		$.post($form.attr("action"), $form.serialize()).then(function() {
-			$(".feedback-input").val("");
+
+	let testForm = document.querySelector("#contact-form-item form");
+      
+	testForm.addEventListener('submit', e => {
+	e.preventDefault();
+	const formData = new FormData(testForm);
+	fetch(testForm.getAttribute('action'), {
+		method: 'POST',
+		headers: {
+		'Accept': 'application/x-www-form-urlencoded;charset=UTF-8',
+		'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+		},
+		body: new URLSearchParams(formData).toString(),  
+	})
+		.then(res => {
+			if (res) {
+					alert("Thank you! Todd will be in touch shortly clarify any details.")
+					$(".feedback-input").val("");
+			}
 		});
-	}
-
-	document.querySelector(".contact-form").addEventListener("submit", submitForm);
+	});
 
 };
